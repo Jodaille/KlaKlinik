c60a681f13abb5aa389896098fea3461e2ea444f
Jodaille <jody.noury@gmail.com>
2019-12-17 23:14:45 +0100

    add some icons backpack

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,9 +1,9 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('species') }}'><i class='nav-icon fa fa-question'></i> Espèces</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('animal') }}'><i class='nav-icon fa fa-question'></i> Animaux</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('location') }}'><i class='nav-icon fa fa-question'></i> Emplacements</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('animal_owner') }}'><i class='nav-icon fa fa-question'></i> Propriétaires</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('animal') }}'><i class='nav-icon fa fa-paw'></i> Animaux</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('species') }}'><i class='nav-icon fa fa-female'></i> Espèces</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('location') }}'><i class='nav-icon fa fa-location-arrow'></i> Emplacements</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('animal_owner') }}'><i class='nav-icon fa fa-user-plus'></i> Propriétaires</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('address') }}'><i class='nav-icon fa fa-question'></i> Adresses</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('timeline') }}'><i class='nav-icon fa fa-question'></i> Emploi du temps</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('operation') }}'><i class='nav-icon fa fa-question'></i> Opérations</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('timeline') }}'><i class='nav-icon fa fa-calendar'></i> Emploi du temps</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('operation') }}'><i class='nav-icon fa fa-ambulance'></i> Opérations</a></li>
