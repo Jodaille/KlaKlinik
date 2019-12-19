3d26df6e15e5f12205de0d54d37b5442131c328d
Jodaille <jody.noury@gmail.com>
2019-12-19 08:31:15 +0100

    style is style

--- resources/lang/vendor/backpack/fr/crud.php
+++ resources/lang/vendor/backpack/fr/crud.php
@@ -117,8 +117,8 @@ return [
         'please_fix'          => 'Please fix the following errors:',
 
     // global crud - success / error notification bubbles
-        'insert_success' => 'The item has been added successfully.',
-        'update_success' => 'The item has been modified successfully.',
+        'insert_success' => 'Ajouté !',
+        'update_success' => 'Modifié !',
 
     // CRUD reorder view
         'reorder'                      => 'Reorder',
