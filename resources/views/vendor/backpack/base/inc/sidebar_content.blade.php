1f40660f86a279522264564802476b84abe8c32c
Jodaille <jody.noury@gmail.com>
2019-12-26 15:04:02 +0100

    add link to home

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="/"><i class="fa fa-dashboard nav-icon"></i> Accueil</a></li>
+<li class="nav-item"><a class="nav-link" href="/"><i class="fa fa-home nav-icon"></i> Accueil</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('animal') }}'><i class='nav-icon fa fa-paw'></i> Animaux</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('species') }}'><i class='nav-icon fa fa-female'></i> Espèces</a></li>
