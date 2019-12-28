79ce388aa6eee97c9849542dfb8cb2f28332a9c9
Jodaille <jody.noury@gmail.com>
2019-12-28 13:48:57 +0100

    margin top

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,11 +1,13 @@
  <div class="c-navbar">
     <div class="o-wrapper">
         <div class="c-navbar__wrapper">
+            <div class="u-margin-top">
             <form action="{{route('search')}}" method="GET">
-            <span><input type="search" name="q" placeholder="" /></span>
+            <span ><input type="search" name="q" placeholder="" /></span>
             <span><button type="submit" name="rechercher" value="rechercher" placeholder="rechercher" ><i class="las la-question-circle"></i></button></span>
             @csrf
             </form>
+            </div>
         </div>
     </div>
 </div>
